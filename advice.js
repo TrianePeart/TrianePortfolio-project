c48226ef75d1a5560ac04226deb51df4a91b9771
TrianePeart <TrianePeart@gmail.com>
2022-09-14 18:00:17 -0400

    Daily Advise Complete

--- advice.js
+++ advice.js
@@ -1,43 +1,28 @@
-//CODE NOT WORKING REDO
 const URL = "https://api.adviceslip.com/advice"
-//This is my URL I like it to be away from everything just because. 
-
+ 
 const advice = document.querySelector("#advice")
-//I'm making a query to find the advice result. I don't like naming things result. 
-const button = document.querySelector("#button")
 
-const body = document.querySelector("Body")
-//This will be my button to call upon it. 
+const button = document.querySelector("#button")
 
 button.addEventListener('click', () => {
     randomAdvice()
+   
 })
-//This should work as a regular button.
-
-//NOTE: Might need an onload but depends Ask Instructor 
 
 function randomAdvice(){
-    //create a function that displays advice when clicked 
-    //NOTE: I want to try to do this without the event being inside it because I couldn't make it work during the weather app. 
     fetch(URL)
     .then(res => res.json())
     .then((data) => {
-        //Making a function to call the slipped. It worked for the weather app so why not?
         const adviceData = data.slip
         //This is calling the random advice from the api slip
        advice.innerHTML = `<p>${adviceData.advice}</p>`
-        //I can't remember who I saw do this... I think it was Edgy. It's basically creating an HTML placement for the advice. The way I did this before was way too complicated. 
 
+       button.style.display="none";
+       //Taking away the button when done.
     })
     .catch((err) => {
         console.log(err)
     })
  
 }
-
-button.removeEventListener("click", () =>{
-    randomAdvice()
-})
-
-//This isn't removing the event. Find solution
 
