7bf63244b5f500fefa6b40bea3f900adc2172b1c
TrianePeart <TrianePeart@gmail.com>
2022-09-13 20:25:05 -0400

    First Modal created (No closing functionality)

--- zodiac.js
+++ zodiac.js
@@ -1,54 +1,13 @@
-//Have a div = display none 
-//a click event listener for card
-//onclick set to display block
-//This will be done in Css 
+const trigger = document.querySelector("button")
+const modal = document.querySelector(".modal")
+const close = document.querySelector(".modal-close")
 
 
-//use a for each 
-//Should be able to call the zodiac cards 
-
-// if a specific page is selected it should navigate to a display page for it. 
-
-//if 1-12 signs it should show different information for them
-
-//should hide signs that are not selected. 
-
-// maybe add hyperlinks if there is time. 
-
-/* 
-
-Maybe need 12 cards per Zodiac then an onClick that brings you to a new page or maybe makes the selected card extend? 
-
-should include following for each 
-
-Element: 
-
-Color: 
-
-Quality: 
-
-Day: 
-
-Ruler: 
-
-Greatest Compatibility: 
-
-Lucky Numbers: 
-
-Dates:
+trigger.addEventListener("click", (event) =>{
+    modal.style.display = "flex";
+    //This is setting the modal display property to flex on click
+})
 
 
-Sign Traits: 
 
-Strengths: 
 
-Sign Weaknesses:
-
-Sign likes: 
-
-Sign dislikes: 
-
-sign info:
-*/
-
-//Likely an log if statement on selection but should ask instructors if pages should be broken up or kept together. 
