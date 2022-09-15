075afd98eea16a6170a07a55ddf168c40d23b72c
TrianePeart <TrianePeart@gmail.com>
2022-09-15 17:36:46 -0400

    Main Complete

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-//MAYBE HAVE SOME KIND OF NAV sound or effect in here. Keep it simple don't do too much. 
+const trigger = document.querySelectorAll("[data-modal-target]")
+const modal = document.querySelectorAll(".modal")
+const close = document.querySelectorAll(".modal-close")
+const logo = document.querySelector("#logo")
+
+trigger.forEach(credit => {
+  credit.addEventListener("click", event => toggle(event.currentTarget.getAttribute("data-modal-target")));
+});
+close.forEach(credit => {
+  credit.addEventListener("click", event => toggle(event.currentTarget.closest(".modal").id));
+});
+modal.forEach(credit => {
+  credit.addEventListener("click", event => {
+    if(event.currentTarget === event.target) toggle(event.currentTarget.id);
+  });
+});
+
+
+function toggle(modalId) {
+  const modals = document.getElementById(modalId);
+
+  if(getComputedStyle(modals).display==="flex") {  
+    modals.classList.add("modal-hide");
+    setTimeout(() => {
+      document.body.style.overflow = "initial"; 
+      modals.classList.remove("modal-show", "modal-hide");
+      modals.style.display = "none";      
+    }, 200);
+  }
+  else {
+    document.body.style.overflow = "hidden"; 
+    modals.style.display = "flex";
+    modals.classList.add("modal-show");
+  }
+}
+
